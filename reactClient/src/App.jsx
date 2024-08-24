d9783de0c90991ddf489ab1336a900d3be878181
GabrielOnohara <gabakio11@gmail.com>
2024-08-24 13:39:12 -0300

    adjust styles

--- reactClient/src/App.jsx
+++ reactClient/src/App.jsx
@@ -1,26 +1,32 @@
-import verzelLogo from './assets/logo2.jpg'
-import './App.css'
-import Button from 'react-bootstrap/Button';
+import verzelLogo from "./assets/logo2.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
-
   return (
-    <>
+    <div className="root">
       <div>
-        <img src={verzelLogo} className="logo" alt="Verzel Movies logo" />
+        <img src={verzelLogo} className="logo my-4" alt="Verzel Movies logo" />
       </div>
-      <h1>Verzel Movies</h1>
-      <div className="card">
-        <Button variant='dark' onClick={() => {console.log('clicou em acessar');
-        }}>
-           Acessar
+      <h1 className="mb-4">Verzel Movies</h1>
+      <div className="">
+        <Button
+          size="lg"
+          variant="light"
+          className="px-4 mt-2"
+          onClick={() => {
+            console.log("clicou em acessar");
+          }}
+        >
+          Acessar
         </Button>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs my-3">
         Clique em acessar para iniciar sua jornada no mundo dos filmes
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- reactClient/src/main.jsx
+++ reactClient/src/main.jsx
@@ -3,8 +3,24 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from './login/Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: '/login/',
+        element: <Login/>
+      }
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
