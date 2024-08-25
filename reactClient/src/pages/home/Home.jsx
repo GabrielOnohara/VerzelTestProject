aa0d88c53924cf3a00eda4a06b6a940162f05d02
GabrielOnohara <gabakio11@gmail.com>
2024-08-25 02:54:24 -0300

    start btn group idea

--- reactClient/src/pages/home/Home.jsx
+++ reactClient/src/pages/home/Home.jsx
@@ -2,11 +2,18 @@ import { useContext } from "react";
 
 import { UserContext } from "../../contexts/UserContext";
 import { useNavigate } from "react-router-dom";
-import { Button, Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import {
+  Button,
+  Container,
+  ButtonGroup,
+  ButtonToolbar,
+  Form,
+  InputGroup,
+} from "react-bootstrap";
 
 import verzelLogo from "../../assets/logo2.jpg";
 import "./Home.css";
-import NavBar from './NavBar'
+import NavBar from "./NavBar";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -44,15 +51,34 @@ const Home = () => {
     );
 
   return (
-    <div className="homeSection">
-      <NavBar user={user}/>
-      <div className="container-sm">
-        <h1>Home</h1>
-        <p className="text-break mx-auto" style={{ width: "50%" }}>
-          {token}
-        </p>
-        {user && <p> {user.username} </p>}
-      </div>
+    <div className="home">
+      <NavBar user={user} />
+      <Container className="homeContent py-5">
+        <ButtonToolbar
+          className="mb-3 mt-3"
+          aria-label="Toolbar with Button groups"
+        >
+          <InputGroup data-bs-theme="dark" className="me-auto">
+            <Form.Control
+              type="text"
+              placeholder="Nome de filme"
+              aria-label="Nome de filme"
+              aria-describedby="btnGroupAddon"
+            />
+            <InputGroup.Text id="btnGroupAddon">
+              <Button variant="outtline-light" >
+                Pesquisar
+              </Button>
+            </InputGroup.Text>
+          </InputGroup>
+          <ButtonGroup className="me-2" aria-label="First group">
+            <Button variant="outline-light">Mais populares</Button>{" "}
+            <Button variant="outline-light">Próximos lançamentos</Button>{" "}
+            <Button variant="outline-light">Melhores avaliações</Button>{" "}
+            <Button variant="outline-light">Exibindo agora</Button>
+          </ButtonGroup>
+        </ButtonToolbar>
+      </Container>
     </div>
   );
 };
