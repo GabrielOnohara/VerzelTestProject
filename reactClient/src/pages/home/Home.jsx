d57e56387a9767067b06314cd5ffb1c5123f5aed
GabrielOnohara <gabakio11@gmail.com>
2024-08-25 02:31:51 -0300

    add navbar component

--- reactClient/src/pages/home/Home.jsx
+++ reactClient/src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ import { Button, Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 
 import verzelLogo from "../../assets/logo2.jpg";
 import "./Home.css";
+import NavBar from './NavBar'
 
 const Home = () => {
   const navigate = useNavigate();
@@ -44,31 +45,7 @@ const Home = () => {
 
   return (
     <div className="homeSection">
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <NavBar user={user}/>
       <div className="container-sm">
         <h1>Home</h1>
         <p className="text-break mx-auto" style={{ width: "50%" }}>
---
+++ reactClient/src/pages/home/NavBar.jsx
@@ -0,0 +1,37 @@
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import PropTypes from "prop-types";
+
+const NavBar = ({ user }) => {
+  return (
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand class-name="mr-auto" href="#home">
+            Vercel Movies
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav.Link className="ms-auto" href="#link">
+              Meus favoritos
+            </Nav.Link>
+            <NavDropdown
+              className="ms-auto"
+              title={user.username ?? ""}
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1">Sair</NavDropdown.Item>
+            </NavDropdown>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+NavBar.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default NavBar;
